351c9a8a0648f0c6c56a870205f70d665b924578
0x70 <greezygreed@gmail.com>
2022-07-24 01:52:05 +0200

    Created basic site layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,27 +3,12 @@ import type { NextPage } from 'next';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Layout from '../components/layout';
 
 const Home: NextPage = () => {
   return (
-    <Container maxWidth='lg'>
-      <Box
-        sx={{
-          my: 5,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Typography component='h1' color='primary'>
-          Material UI v5 with Next.js in TypeScript
-        </Typography>
-        <Typography component='h2' color='secondary'>
-          Boilerplate for building faster.
-        </Typography>
-      </Box>
-    </Container>
+    <Layout>
+    </Layout>
   );
 };
 
