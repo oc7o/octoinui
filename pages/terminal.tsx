21671c8734d4c5af07df34669f1e09a25d0616c3
0x70 <greezygreed@gmail.com>
2022-07-29 17:16:59 +0200

    Created Terminal route

---
+++ pages/terminal.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next';
+import Layout from '../components/layout';
+
+
+const Terminal: NextPage = () => {
+
+    return (
+      <Layout>
+        
+      </Layout>
+    );
+  };
+  
+  export default Terminal;
+  
