3f5302468fb43491bb612a8708a66d6c94937096
0x70 <greezygreed@gmail.com>
2022-08-18 19:59:50 +0200

    Created first preview

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Products from "../components/Products.svelte";
+  import Carousel from "../components/Carousel.svelte";
+</script>
+
+<div class="flex justify-center my-16">
+  <p
+    class="box-decoration-clone bg-gradient-to-r from-lime-500 to-emerald-500 text-white text-5xl font-bold px-12 py-3"
+  >
+    Unsere Top Künstler
+  </p>
+</div>
+
+<!-- <div class="mx-36 my-12"> -->
+<Carousel />
+<!-- </div> -->
+
+<div class="flex justify-center my-16">
+  <p
+    class="box-decoration-clone bg-gradient-to-r from-lime-500 to-emerald-500 text-white text-5xl font-bold px-12 py-3"
+  >
+    Die innovativsten Produkte
+  </p>
+</div>
+
+<div class="">
+  <Products />
+</div>
+<br />
