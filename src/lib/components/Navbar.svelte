0f98aa4f80a623789da1955b7b11574bd801e817
0x70 <greezygreed@gmail.com>
2023-03-11 15:32:17 +0100

    From auth commit

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,125 +1,143 @@
-<script>
-  import { authStore } from "../stores/authStore";
-  import { userStore } from "../stores/userStore";
+<script lang="ts">
+	import { graphql } from '$houdini';
+
+	$: meQuery = graphql(`
+		query NavbarQuery {
+			me {
+				username
+				isStaff
+				isActive
+				isSuperuser
+				email
+				lastName
+				firstName
+				dateJoined
+				lastLogin
+				profileImage
+			}
+		}
+	`);
 </script>
 
+<!-- {JSON.stringify($meQuery?.data?.me)} -->
+
 <div class="navbar bg-gradient-to-r from-lime-500 to-emerald-500">
-  <!-- ⬆ text-white -->
-  <!-- Navbar Start -->
-  <div class="navbar-start">
-    <div class="dropdown">
-      <label for="side-drawer" class="btn btn-primary drawer-button">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          class="inline-block w-5 h-5 stroke-current"
-          ><path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M4 6h16M4 12h16M4 18h16"
-          /></svg
-        >
-      </label>
-      <ul
-        tabindex="0"
-        class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-      >
-        <li><a href="/">Homepage</a></li>
-        <li><a>Portfolio</a></li>
-        <li><a href="/about">About</a></li>
-      </ul>
-    </div>
-  </div>
-  <!-- Navbar Start End -->
+	<!-- ⬆ text-white -->
+	<!-- Navbar Start -->
+	<div class="navbar-start">
+		<div class="dropdown">
+			<label for="side-drawer" class="btn btn-primary drawer-button">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					class="inline-block w-5 h-5 stroke-current"
+					><path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M4 6h16M4 12h16M4 18h16"
+					/></svg
+				>
+			</label>
+			<ul
+				tabindex="0"
+				class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+			>
+				<li><a href="/">Homepage</a></li>
+				<li><a>Portfolio</a></li>
+				<li><a href="/about">About</a></li>
+			</ul>
+		</div>
+	</div>
+	<!-- Navbar Start End -->
 
-  <!-- Navbar Center  -->
-  <div class="navbar-center">
-    <a class="btn btn-ghost normal-case text-xl" href="/">Sloow</a>
-  </div>
-  <!-- Navbar Center End-->
+	<!-- Navbar Center  -->
+	<div class="navbar-center">
+		<a class="btn btn-ghost normal-case text-xl" href="/">Sloow</a>
+	</div>
+	<!-- Navbar Center End-->
 
-  <!-- Navbar End -->
-  <div class="navbar-end">
-    <!-- Search Button -->
-    <button class="btn btn-ghost btn-circle">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-5 w-5"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        ><path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-        /></svg
-      >
-    </button>
-    <!-- Search Button End -->
+	<!-- Navbar End -->
+	<div class="navbar-end">
+		<!-- Search Button -->
+		<button class="btn btn-ghost btn-circle">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="h-5 w-5"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor"
+				><path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+				/></svg
+			>
+		</button>
+		<!-- Search Button End -->
 
-    <!-- Shopping Cart -->
-    <div class="dropdown dropdown-end">
-      <span tabindex="0" class="btn btn-ghost btn-circle">
-        <div class="indicator">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            ><path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-            /></svg
-          >
-          <!-- <span class="badge badge-sm indicator-item">8</span> -->
-        </div>
-      </span>
-      <div
-        tabindex="0"
-        class="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow"
-      >
-        <div class="card-body">
-          <span class="font-bold text-lg">8 Items</span>
-          <span class="text-info">Subtotal: $999</span>
-          <div class="card-actions">
-            <a class="btn btn-primary btn-block" href="/cart">View cart</a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- Shopping Cart End -->
+		<!-- Shopping Cart -->
+		<div class="dropdown dropdown-end">
+			<span tabindex="0" class="btn btn-ghost btn-circle">
+				<div class="indicator">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-5 w-5"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+						><path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+						/></svg
+					>
+					<!-- <span class="badge badge-sm indicator-item">8</span> -->
+				</div>
+			</span>
+			<div tabindex="0" class="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow">
+				<div class="card-body">
+					<span class="font-bold text-lg">8 Items</span>
+					<span class="text-info">Subtotal: $999</span>
+					<div class="card-actions">
+						<a class="btn btn-primary btn-block" href="/basket">View basket</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- Shopping Cart End -->
 
-    {#if $authStore.loggedIn}
-      <!-- User Account -->
-      <div class="dropdown dropdown-end">
-        <span tabindex="0" class="btn btn-ghost btn-circle avatar">
-          <div class="w-10 rounded-full">
-            <img src={$userStore?.profileImage} />
-          </div>
-        </span>
-        <ul
-          tabindex="0"
-          class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-        >
-          <li>
-            <a class="justify-between"> Profile </a>
-          </li>
-          <li><a>Settings</a></li>
-          <li><a href="/new-product">Create a New Produkt</a></li>
-          <li><a href="/logout">Logout</a></li>
-        </ul>
-      </div>
-      <!-- User Account End -->
-    {:else}
-      <!-- Login Button -->
-      <a class="btn btn-accent" href="/login">Login</a>
-      <!-- Login Button End -->
-    {/if}
-  </div>
+		{#if $meQuery?.data?.me !== null}
+			<!-- User Account -->
+			<div class="dropdown dropdown-end">
+				<span tabindex="0" class="btn btn-ghost btn-circle avatar">
+					<div class="w-10 rounded-full">
+						<!-- <img src={$userStore?.profileImage} /> -->
+					</div>
+				</span>
+				<ul
+					tabindex="0"
+					class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+				>
+					<li>
+						<a class="justify-between" href={`/users/${$meQuery?.data?.me.username}`}> Profile </a>
+					</li>
+					<li>
+						<a class="justify-between"> Account </a>
+					</li>
+					<li><a>Settings</a></li>
+					<li><a href="/new-product">Create a New Produkt</a></li>
+					<li><form method="POST" action="/user?/logout"><button>Logout</button></form></li>
+				</ul>
+			</div>
+			<!-- User Account End -->
+		{:else}
+			<!-- Login Button -->
+			<a class="btn btn-accent" href="/user/login">Login</a>
+			<!-- Login Button End -->
+		{/if}
+	</div>
 </div>
