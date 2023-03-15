0c7812452262d5a99d2be94805a235a6efb86a54
0x70 <greezygreed@gmail.com>
2023-03-15 21:38:58 +0100

    Started implementing Product List

---
+++ src/lib/components/ProductList.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { graphql } from '$houdini';
+
+	$: limit = 12;
+	$: offset = 0;
+	let search: string | null = null;
+
+	export const _ProductListQueryVariables = ({}) => {
+		return { user: props?.user ? props.user : null, offset, limit, search };
+	};
+
+	$: productListQuery = graphql(`
+		query ProductsQuery($user: String = null, $offset: Int, $limit: Int, $search: String = null) {
+			products(user: $user, offset: $offset, limit: $limit, search: $search) @paginate {
+				items {
+					webId
+					name
+					defaultImage {
+						imgUrl
+					}
+				}
+				totalItemsCount
+			}
+		}
+	`);
+</script>
