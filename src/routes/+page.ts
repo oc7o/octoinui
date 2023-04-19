1801c8b00e94934965de7a63c3cfc3bedc26d3a7
0x70 <greezygreed@gmail.com>
2023-04-19 20:07:24 +0200

    Moved files

---
+++ src/routes/+page.ts
@@ -0,0 +1,32 @@
+import { graphql } from '$houdini';
+
+export const _houdini_load = graphql`
+	query Home($user: String = null, $offset: Int, $limit: Int, $search: String = null) {
+		products(user: $user, offset: $offset, limit: $limit, search: $search) {
+			items {
+				webId
+				name
+				description
+				startingFromPrice
+				defaultImage {
+					imgUrl
+				}
+			}
+			totalItemsCount
+		}
+		top20Users {
+			username
+			isSuperuser
+			isStaff
+			profileImage
+		}
+	}
+`;
+
+export const _HomeVariables = ({ url }) => {
+	return {
+		offset: Number(url.searchParams.get('offset')) || 0,
+		limit: 12,
+		search: url.searchParams.get('search') || null
+	};
+};
