342facba2d1d7d75137ecfeea5a370c262d02518
0x70 <greezygreed@gmail.com>
2023-04-19 20:01:27 +0200

    Added notifications

---
+++ src/lib/components/Toast.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	import { fade } from 'svelte/transition';
+	import Close from '$lib/components/icons/Close.svelte';
+
+	const dispatch = createEventDispatcher();
+
+	export let type = 'error';
+	export let dismissible = true;
+</script>
+
+<div class="alert alert-{type}" transition:fade>
+	{#if type === 'success'}{:else if type === 'error'}{:else}{/if}
+
+	<slot />
+
+	{#if dismissible}
+		<button class="close" on:click={() => dispatch('dismiss')}><Close /></button>
+	{/if}
+</div>
