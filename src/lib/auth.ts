0ae996c471ce1ad64d76854a1a40d072caba176f
0x70 <greezygreed@gmail.com>
2023-05-08 14:14:59 +0200

    Added refresh functionality to auth sys

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,7 @@
 import type { RequestEvent } from '@sveltejs/kit';
 import { MeQueryStore } from '$houdini';
 import { writable } from 'svelte/store';
-import { setSession } from '$houdini';
+import { setSession, graphql } from '$houdini';
 
 export const authenticateUser = async (event: RequestEvent) => {
 	// get the cookies from the request
@@ -13,13 +13,41 @@ export const authenticateUser = async (event: RequestEvent) => {
 	setSession(event, { auth: { accessToken, refreshToken } });
 
 	const myQuery = new MeQueryStore();
-	const { data } = await myQuery.fetch({ event });
+	const res1 = await myQuery.fetch({ event });
 
-	if (!data?.me) {
+	if (!res1.data?.me) {
+		if (refreshToken != null && refreshToken != '') {
+			const refreshMutation: any = graphql`
+				mutation newAccessTokenFromRefreshToken($refreshToken: String) {
+					refreshToken(refreshToken: $refreshToken) {
+						token
+					}
+				}
+			`;
+			const res2 = await refreshMutation.mutate({ refreshToken }, { event });
+
+			if (res2.errors) {
+				cookies.delete('token');
+				cookies.delete('refreshToken');
+				return null;
+			}
+			if (res2.data && res2.data.refreshToken && res2.data.refreshToken.token) {
+				cookies.set('token', res2.data.refreshToken.token);
+
+				setSession(event, { auth: { accessToken: res2.data.refreshToken.token, refreshToken } });
+				const myQuery = new MeQueryStore();
+				const res3 = await myQuery.fetch({ event });
+
+				if (res3.data?.me) {
+					return res3.data.me;
+				}
+			}
+		}
+
 		return null;
 	} else {
 		const user = {
-			...data?.me
+			...res1.data?.me
 		};
 
 		return user;
